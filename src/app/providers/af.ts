98332594d4241c05520a29700a1b1951c4324ab7
Niels Odgaard <niel2647@edu.easj.dk>
2017-04-02 23:27:02 +0200

    Chat works

--- src/app/providers/af.ts
+++ src/app/providers/af.ts
@@ -1,9 +1,20 @@
 // src/app/providers/af.ts
 import {Injectable} from "@angular/core";
-import {AngularFire, AuthProviders, AuthMethods} from 'angularfire2';
+import {AngularFire, AuthProviders, AuthMethods, FirebaseListObservable} from 'angularfire2';
+
 @Injectable()
 export class AF {
-  constructor(public af: AngularFire) {}
+  public messages: FirebaseListObservable<any>;
+  public users: FirebaseListObservable<any>;
+  public displayName: string;
+  public email: string;
+
+  constructor(public af: AngularFire) {
+    this.messages = this.af.database.list('messages');
+    this.users = this.af.database.list('users');
+  }
+
+
   /**
    * Logs in the user
    * @returns {firebase.Promise<FirebaseAuthState>}
@@ -14,16 +25,47 @@ export class AF {
       method: AuthMethods.Popup,
     });
   }
+
+  /**
+   * Logs in the user
+   * @returns {firebase.Promise<FirebaseAuthState>}
+   */
   loginWithFacebook() {
       return this.af.auth.login({
           provider: AuthProviders.Facebook,
           method: AuthMethods.Popup,
       })
   }
+
   /**
    * Logs out the current user
    */
   logout() {
     return this.af.auth.logout();
+  }
+
+  /**
+   *
+   */
+  addUserInfo(){
+    //We saved their auth info now save the rest to the db.
+    this.users.push({
+      email: this.email,
+      displayName: this.displayName
+    });
+  }
+
+  /**
+   * Saves a message to the Firebase Realtime Database
+   * @param text
+   */
+  sendMessage(text) {
+    var message = {
+      message: text,
+      displayName: this.displayName,
+      email: this.email,
+      timestamp: Date.now()
+    };
+    this.messages.push(message);
   }
 }
